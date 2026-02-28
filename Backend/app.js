15c67fece68f4bad8e3ab79388fdf1500d85905a
Jagdish Choudhary <jagdishchodhary426@gmail.com>
2026-02-28 23:09:27 +0530

    GET Route Added

--- Backend/app.js
+++ Backend/app.js
@@ -1,27 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectdb from "./src/config/mongo.config.js";
-import urlSchema from "./src/models/shorturl.model.js";
-import {nanoid} from "nanoid";
-dotenv.config("./.env");
-const app = express();
+import Url from "./src/models/shorturl.model.js";
+import { nanoid } from "nanoid";
 
+dotenv.config();
+const app = express();
 app.use(express.json());
 connectdb();
 
 app.post("/create", async (req, res) => {
     try {
         const { url } = req.body;
-        if (!url){
+        if (!url) {
             return res.status(400).json({ message: "URL is required" });
-        } 
+        }
         const shortUrl = nanoid(7);
-        const newUrl = new urlSchema({
+        const newUrl = new Url({
             full_url: url,
             short_url: shortUrl,
-        })
-        res.send(nanoid(7));
-        newUrl.save();
+        });
+        await newUrl.save();
+        res.status(201).json({
+            shortUrl: shortUrl
+        });
     } 
     catch (error) {
         console.error(error);
@@ -30,9 +32,19 @@ app.post("/create", async (req, res) => {
 });
 
 app.get("/:id", async (req, res) => {
-    const {id} = req.params()
-    const url = await urlSchema.findOne({short_url: shortUrl})
-})
+    try {
+        const { id } = req.params;
+        const urlDoc = await Url.findOne({ short_url: id });
+        if (!urlDoc) {
+            return res.status(404).json({ message: "URL not found" });
+        }
+        res.redirect(urlDoc.full_url);
+    } 
+    catch (error) {
+        res.status(500).json({ message: "Server Error" });
+    }
+});
+
 app.listen(3000, () => {
     console.log("Server is running successfully.");
 });
