8f25e85988c342a3a847b6dba796a7b84247386d
Jagdish Choudhary <jagdishchodhary426@gmail.com>
2026-03-28 18:42:12 +0530

    Add User Model

--- Backend/src/config/mongo.config.js
+++ Backend/src/config/mongo.config.js
@@ -1,13 +1,17 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+console.log(process.env.MONGO_URI);
 
-const connection = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch(error){
-        console.log(`Error: ${error.message}`);
-        process.exit(1);
-    }
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
 
-export default connection;
+export default connectDB;
